813fe1c01f7119c070880543d5e73ff467de5e27
maier-c <reyesmaier@gmail.com>
2022-09-23 21:40:11 -0500

    menu updating

--- app.js
+++ app.js
@@ -1,32 +1,25 @@
-const express = require('express');
-const path = require('path');
+const express = require("express");
+const path = require("path");
 const app = express();
 
-app.use(express.static('public'))
+app.use(express.static("public"));
 
-app.get('/', (req,res) => {
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "./src/views/home.html"));
+});
 
-    res.sendFile(path.join(__dirname, './src/views/home.html'));
+app.get("/register", (req, res) => {
+  res.sendFile(path.join(__dirname, "./src/views/register.html"));
+});
 
-})
+app.get("/login", (req, res) => {
+  res.sendFile(path.join(__dirname, "./src/views/login.html"));
+});
 
-app.get('/register', (req,res) => {
-
-    res.sendFile(path.join(__dirname, './src/views/register.html'));
-
-})
-
-app.get('/login', (req,res) => {
-
-    res.sendFile(path.join(__dirname, './src/views/login.html'));
-})
-
-app.get('/productDetail', (req,res) => {
-
-    res.sendFile(path.join(__dirname, './src/views/productDetail.html'));
-
-})
+app.get("/productDetail", (req, res) => {
+  res.sendFile(path.join(__dirname, "./src/views/productDetail.html"));
+});
 
 app.listen(3030, () => {
-    console.log('Inicializado el servidor');
-})
+  console.log("Inicializado el servidor");
+});
