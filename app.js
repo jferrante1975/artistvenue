0244eef03caca0735d1275f388e6afc739d2368b
Maier <maier@localhost.localdomain>
2022-10-04 21:16:59 -0500

    install ejs engine

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const path = require("path");
 const app = express();
 
 app.use(express.static("public"));
+app.set('view engine', 'ejs');
 
 app.get("/", (req, res) => {
   res.sendFile(path.join(__dirname, "./src/views/home.html"));
