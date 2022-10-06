00e1019da700da62b0d89a48d75270cfe1a4436c
Maier CR <reyesmaier@gmail.com>
2022-10-05 19:39:19 -0500

    Separate views into folders

--- app.js
+++ app.js
@@ -1,32 +1,16 @@
 const express = require("express");
 const path = require("path");
 const app = express();
+const mainRoutes = require("./src/routes/mainRoutes");
+const PORT = 3000;
 
-app.use(express.static("public"));
-app.set('view engine', 'ejs');
+app.use(express.static(path.join(__dirname, '/public')));
+app.set("view engine", "ejs");
 
-app.get("/", (req, res) => {
-  res.sendFile(path.join(__dirname, "./src/views/home.html"));
-});
+app.use(express.static(path.join(__dirname, "/public")));
 
-app.get("/register", (req, res) => {
-  res.sendFile(path.join(__dirname, "./src/views/register.html"));
-});
+app.use("/", mainRoutes);
 
-app.get("/login", (req, res) => {
-  res.sendFile(path.join(__dirname, "./src/views/login.html"));
-});
-
-app.get("/productDetail", (req, res) => {
-  res.sendFile(path.join(__dirname, "./src/views/productDetail.html"));
-});
-
-app.get('/car', (req,res) => {
-
-    res.sendFile(path.join(__dirname, './src/views/car.html'));
-
-})
-
-app.listen(3030, () => {
-  console.log("Inicializado el servidor");
+app.listen(PORT, () => {
+  console.log(`Servidor inicializado en el puerto: ${PORT}...`);
 });
