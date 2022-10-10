973dbaa369cfb527c3d6b0970fc1874932692685
Maier CR <reyesmaier@gmail.com>
2022-10-10 18:34:10 -0500

    edit register link on header

--- src/routes/usersRoutes.js
+++ src/routes/usersRoutes.js
@@ -3,6 +3,7 @@ const router = express.Router();
 const userController = require("../controllers/userController");
 
 router.get("/login", userController.login);
+router.get("/register", userController.register);
 router.get("/", userController.users);
 // router.get("/userDetail", userController.detail);
 
