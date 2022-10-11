c60b7c7dd3dab5963b445de9f2fcd69a1256ed87
Maier CR <reyesmaier@gmail.com>
2022-10-10 19:29:20 -0500

    working on cart module...

--- src/controllers/productController.js
+++ src/controllers/productController.js
@@ -30,6 +30,10 @@ const productController = {
   },
   detail: (req, res) => {
     res.render('products/productDetail');
+  },
+
+  cart: (req, res) => {
+    res.render('products/cart');
   }
 };
 
--- src/routes/productsRoutes.js
+++ src/routes/productsRoutes.js
@@ -4,6 +4,6 @@ const productController = require("../controllers/productController");
 
 router.get("/", productController.products);
 router.get("/productDetail", productController.detail);
-
+router.get("/cart", productController.cart);
 
 module.exports = router;
