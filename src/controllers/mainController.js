28004f0c11d5278eefaa33f7b88e7f88d3a3dd3a
Maier CR <reyesmaier@gmail.com>
2022-10-11 20:24:01 -0500

    add dynamic views title

--- src/controllers/mainController.js
+++ src/controllers/mainController.js
@@ -2,6 +2,7 @@ const path = require("path");
 
 const mainController = {
   home: (req, res) => {
+    res.locals.title = "Home"; 
     res.render(path.join(__dirname, "../views/home.ejs"));
   },
 };
--- src/controllers/productController.js
+++ src/controllers/productController.js
@@ -1,44 +1,45 @@
 const path = require("path");
-const fs   = require('fs');
+const fs = require("fs");
 
-
-const productsFilePath = path.join(__dirname, '../data/productDataBase.json');
-const products = JSON.parse(fs.readFileSync(productsFilePath, 'utf-8'));
+const productsFilePath = path.join(__dirname, "../data/productDataBase.json");
+const products = JSON.parse(fs.readFileSync(productsFilePath, "utf-8"));
 
 const productController = {
   products: (req, res) => {
-
     const categoria = req.query.categoria;
 
     let producto = {};
 
     if (categoria != null) {
-       producto = products.filter(producto => {
-          if (producto.categoria != null && producto.video != null){
-              return producto.categoria.toLowerCase() == categoria;
-          }
-      } )    
-    }
-    else {
+      producto = products.filter((producto) => {
+        if (producto.categoria != null && producto.video != null) {
+          return producto.categoria.toLowerCase() == categoria;
+        }
+      });
+    } else {
       producto = products;
     }
     
-    res.render(path.join(__dirname, "../views/products/products.ejs"),{producto}); 
-
+    res.locals.title = "Products"; 
+    res.render(path.join(__dirname, "../views/products/products.ejs"), {
+      producto,
+    });
   },
 
   detail: (req, res) => {
-
     const productId = req.params.id;
-    
-    let producto = products.find(producto => {
-      return producto.id == productId;
-    })
 
-    res.render(path.join(__dirname, "../views/products/productDetail.ejs"),{producto}); 
-    
+    let producto = products.find((producto) => {
+      return producto.id == productId;
+    });
+
+    res.locals.title = "Product Detail"; 
+    res.render(path.join(__dirname, "../views/products/productDetail.ejs"), {
+      producto
+    });
   },
   cart: (req, res) => {
+    res.locals.title = "Cart"; 
     res.render(path.join(__dirname, "../views/products/cart.ejs"));
   }
 };
