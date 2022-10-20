2d2548bbc395219196817bc72e740e0cd31551d1
Maier CR <reyesmaier@gmail.com>
2022-10-20 17:02:33 -0500

    express-session deprecated message fixed

--- app.js
+++ app.js
@@ -1,23 +1,30 @@
 const express = require("express");
 const path = require("path");
-const methodOverride =  require('method-override'); 
+const methodOverride = require("method-override");
 const app = express();
 const mainRoutes = require("./src/routes/mainRoutes");
 const productsRoutes = require("./src/routes/productsRoutes");
 const usersRoutes = require("./src/routes/usersRoutes");
-const session = require('express-session');
+const session = require("express-session");
 const PORT = 3000;
 
-app.use(express.static(path.join(__dirname, '/public')));
+app.use(express.static(path.join(__dirname, "/public")));
 app.set("view engine", "ejs");
-app.set('views',path.join(__dirname,'views'));
+app.set("views", path.join(__dirname, "views"));
 
-app.use(methodOverride('_method')); 
+app.use(methodOverride("_method"));
 app.use("/", mainRoutes);
 app.use("/products", productsRoutes);
 app.use("/search", productsRoutes);
 app.use("/users", usersRoutes);
-app.use(session({ secret: 'ArtistVenue secret'}))
+app.use(
+  session({
+    secret: "V3n3uArt15t2022",
+    resave: true,
+    saveUninitialized: true,
+    cookie: { secure: false },
+  })
+);
 
 app.listen(PORT, () => {
   console.log(`Servidor inicializado en el puerto: ${PORT}...`);
